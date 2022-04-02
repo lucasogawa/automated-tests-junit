ce3145ea4e7ecc13d1ffa4b1d6ffaf1caee31335
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-01 21:29:47 -0300

    Refactoring Tests With @BeforeEach

--- src/test/java/com/ogawalucas/automatedtestsjunit/service/ReadjustmentServiceTest.java
+++ src/test/java/com/ogawalucas/automatedtestsjunit/service/ReadjustmentServiceTest.java
@@ -2,6 +2,7 @@ package com.ogawalucas.automatedtestsjunit.service;
 
 import com.ogawalucas.automatedtestsjunit.model.Employee;
 import com.ogawalucas.automatedtestsjunit.service.enums.EPerformance;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -11,35 +12,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReadjustmentServiceTest {
 
-    private static final ReadjustmentService service = new ReadjustmentService();
+    private ReadjustmentService service;
+    private Employee employee;
+
+    @BeforeEach
+    public void initializr() {
+        this.service = new ReadjustmentService();
+        this.employee = new Employee(null, null, BigDecimal.valueOf(1000));
+    }
 
     @Test
     public void mustReadjustment3PerCentOfSalaryIfPerformanceIsBad() {
+        service.readjust(employee, EPerformance.BAD);
+
         var expected = BigDecimal.valueOf(1030).setScale(2, RoundingMode.HALF_UP);
-        var actual = new Employee(null, null, BigDecimal.valueOf(1000));
+        var actual = employee.getSalary();
 
-        service.readjust(actual, EPerformance.BAD);
-
-        assertEquals(expected, actual.getSalary());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void mustReadjustment15PerCentOfSalaryIfPerformanceIsGood() {
+        service.readjust(employee, EPerformance.GOOD);
+
         var expected = BigDecimal.valueOf(1150).setScale(2, RoundingMode.HALF_UP);
-        var actual = new Employee(null, null, BigDecimal.valueOf(1000));
+        var actual = employee.getSalary();
 
-        service.readjust(actual, EPerformance.GOOD);
-
-        assertEquals(expected, actual.getSalary());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void mustReadjustment20PerCentOfSalaryIfPerformanceIsGreat() {
+        service.readjust(employee, EPerformance.GREAT);
+
         var expected = BigDecimal.valueOf(1200).setScale(2, RoundingMode.HALF_UP);
-        var actual = new Employee(null, null, BigDecimal.valueOf(1000));
+        var actual = employee.getSalary();
 
-        service.readjust(actual, EPerformance.GREAT);
-
-        assertEquals(expected, actual.getSalary());
+        assertEquals(expected, actual);
     }
 }
